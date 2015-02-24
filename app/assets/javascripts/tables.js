4dd58d3522911914eb86cec0f177009bdbc61b35
Vignesh Prakasam <vprakasam@anpi.com>
2015-02-24 17:45:55 +0530

    still debugging

--- app/assets/javascripts/tables.js
+++ app/assets/javascripts/tables.js
@@ -1,3 +1,4 @@
+//encoding: utf-8
 //data table initializations
 $(document).ready(function(){
     $("#jobs_table").dataTable({
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,4 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-# Don't declare `role :all`, it's a meta role
+#encoding: utf-8
 role :app, %w{ubuntu@ec2-54-69-3-56.us-west-2.compute.amazonaws.com}
 role :web, %w{ubuntu@ec2-54-69-3-56.us-west-2.compute.amazonaws.com}
 role :db,  %w{ubuntu@ec2-54-69-3-56.us-west-2.compute.amazonaws.com}
